55a9e376c11da1422b1141649310ec19f18921c1
elzdar <73895746+elzdar@users.noreply.github.com>
2020-11-04 20:45:28 +0800

    added home page

--- __main__.py
+++ __main__.py
@@ -3,8 +3,9 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route("/")
-def index():
-    return flask.render_template("content/view_bugs.html")
+@app.route("/home")
+def home():
+    return flask.render_template("content/home.html")
 
 @app.route("/aboutus")
 def aboutus():
