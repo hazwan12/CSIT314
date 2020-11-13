f79012fc51239d2ca7cb97755d3b50262e4c4b84
hazwan <hazwan@aisingapore.org>
2020-11-13 16:47:21 +0800

    Updating database and schema creation script

--- bugzilla/schema.sql
+++ bugzilla/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-	email INTEGER PRIMARY KEY,
+	email TEXT PRIMARY KEY,
 	full_name TEXT NOT NULL,
 	phone_no TEXT NOT NULL,
 	password TEXT NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE user (
 );
 
 CREATE TABLE bug (
-    id INTEGER PRIMARY KEY AUTO INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
 	reporter TEXT NOT NULL,
 	title TEXT NOT NULL,
 	description TEXT NOT NULL,
