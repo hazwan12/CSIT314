43ad56c659dbd013474c4e4a4d53e82399023c30
hazwan12 <36067775+hazwan12@users.noreply.github.com>
2020-11-14 14:11:13 +0800

    Login, registration and session

--- __main__.py
+++ __main__.py
@@ -1,41 +1,68 @@
+import os
 import flask
-import bugzilla
+from bugzilla import sessions
+from bugzilla.models import Bug, User
 
 app = flask.Flask(__name__)
+app.secret_key = os.urandom(12)
+session = sessions.Sessions()
 
 @app.route("/")
+def start():
+    return flask.redirect(flask.url_for('index'))
+
+@app.route("/index")
 def index():
-    return flask.render_template("content/view_bugs.html")
+    bug = Bug()
+    return flask.render_template("content/view_bugs.html", session=session, bug_cards=bug.to_html_cards())
 
 @app.route("/aboutus")
 def aboutus():
-    return flask.render_template("content/about.html")
+    return flask.render_template("content/about.html", session=session)
 
 @app.route("/logout")
 def logout():
-    return flask.render_template("auth/login.html")
+    flask.flash("Logout successfully", 'success')
+    global session 
+    session = sessions.Sessions()
+    return flask.render_template("auth/login.html", session=session)
 
 @app.route("/login")
 def login():
-    return flask.render_template("auth/login.html")
+    return flask.render_template("auth/login.html", session=session)
 
 @app.route("/register")
 def register():
-    return flask.render_template("auth/register.html")
+    return flask.render_template("auth/register.html", session=session)
 
 @app.route("/search/<value>")
 def search(value):
     return "Searching {}".format(value)
 
+@app.route("/do_register", methods=['POST'])
+def do_register():
+    user = User()
+    result = user.registration(flask.request.form["inputEmail"], flask.request.form["inputName"], flask.request.form["inputPhone"], flask.request.form["inputPassword"])
+    if result[0]:
+        flask.flash(result[1], 'success')
+        session.user = flask.request.form["inputEmail"]
+        return flask.redirect(flask.url_for('login'))
+    else:
+        flask.flash(result[1], 'error')
+        return flask.redirect(flask.url_for('register'))
+
 @app.route("/do_login", methods=['POST'])
 def do_login():
-    user = bugzilla.models.User()
-    result = user.check_login(flask.request.get("inputEmail"), flask.request.get("inputPassword"))
+    user = User()
+    result = user.check_login(flask.request.form["inputEmail"], flask.request.form.get("inputPassword"))
 
-    if result:
-        return "Login Succeeded"
+    if result[0]:
+        flask.flash(result[1], 'success')
+        session.user = flask.request.form["inputEmail"]
+        return flask.redirect(flask.url_for('index'))
     else:
-        return "Login Failed"
+        flask.flash(result[1], 'error')
+        return flask.redirect(flask.url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- bugzilla/models.py
+++ bugzilla/models.py
@@ -34,19 +34,47 @@ class User(Model):
     def gen_insert(self, email : str, full_name : str, phone_no : str, password : str, created_dt : str = str(datetime.datetime.now())):
         return """
         INSERT INTO {} (email, full_name, phone_no, password, created_dt) 
-        VALUES ({}, {}, {}, {}, {})
+        VALUES ('{}', '{}', '{}', '{}', '{}')
         """.format(self.table_name, email, full_name, phone_no, password, created_dt)
 
     def check_login(self, email : str, password : str):
         cursor = self.conn.cursor()
-        cursor.execute("select count(*) from user where email = '{}' and password = '{}'".format(email, password))
+
+        #Check if user exists
+        cursor.execute("select count(1) from user where email = '{}'".format(email))
+        result = cursor.fetchall()
+
+        if result[0][0] == 0:
+            return False, "User {} does not exist. Please register".format(email)
+
+        else:
+            cursor.execute("select count(1) from user where email = '{}' and password = '{}'".format(email, password))
+            result = cursor.fetchall()
+
+            if result[0][0] == 0 :
+                return False, "Error in credentials. Please try again"
+            else:
+                return True, "Login Suceeded, Welcome {}!".format(email)
+
+    def registration(self, email : str, full_name : str, phone_no : str, password : str):
+        cursor = self.conn.cursor()
+
+        #Check if user exists
+        cursor.execute("select count(1) from user where email = '{}'".format(email))
         result = cursor.fetchall()
 
         if result[0][0] == 1:
-            return True
+            return False, "User {} is already registered. Please login".format(email)
         else:
-            return False
-
+            try:
+                cursor.execute(self.gen_insert(email, full_name, phone_no, password))
+                self.conn.commit()
+            except Exception as e:
+                print(e)
+                return False, "Error in user creation, please try again"
+            else:
+                return True, "Account created successfully, please proceed to login"
+            
 class Bug(Model):
     table_name = "bug"
     def __init__(self):
@@ -57,3 +85,40 @@ class Bug(Model):
         INSERT INTO {} (reporter, title, description, created_dt) 
         VALUES ({}, {}, {}, {})
         """.format(self.table_name, reporter, title, description, created_dt)
+
+    def to_html_cards(self):
+
+        def gen_card(row):
+            reporter = row[1]
+            title = row[2]
+            description = row[3]
+            created_dt = row[4]   
+            return """
+                <div class="card" style="width: 25rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Reported By : {}</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">Reported On : {}</h6>
+                        <p class="card-text">{}</p>
+                        <a href="#" class="card-link">View Details</a>
+                    </div>
+                </div>
+            """.format(title, reporter, created_dt, description)
+
+        cursor = self.conn.cursor()
+        cursor.execute("select * from bug")
+        result = cursor.fetchall()
+        
+        i = 0
+        bug_card_list = [] 
+        for row in result:
+            if i == 0:
+                bug_card_list.append('<div class="card-deck">')
+            bug_card_list.append(gen_card(row))
+            i += 1
+
+            if i == 3:
+                bug_card_list.append("</div><br>")
+                i=0
+
+        return " ".join(bug_card_list)
